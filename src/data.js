35b3642c5fac10b72b1cd4b99fc68a8cc5729911
raechel <odom.raechel@gmail.com>
2019-06-13 16:47:02 -0600

    init commit

---
+++ src/data.js
@@ -0,0 +1,272 @@
+blues = [
+  {
+    name: 'Ivan  Barbashev',
+    number: 49,
+    position: 'center',
+    height: '6\' 0"',
+    weight: 187,
+    born: 'December 14, 1995',
+    birthplace: 'Moscow, RUS'
+  },
+  {
+    name: 'Sammy Blais',
+    number: 9,
+    position: 'left wing',
+    height: '6\' 2"',
+    weight: 205,
+    born: 'June 17, 1996',
+    birthplace: 'Montmagny, QC, CAN'
+  },
+  {
+    name: 'Tyler Bozak',
+    number: 21,
+    position: 'center',
+    height: '6\' 1"',
+    weight: 199,
+    born: 'March 19, 1986',
+    birthplace: 'Regina, SK, CAN'
+  },
+  {
+    name: 'Robbi Fabri',
+    number: 15,
+    position: 'center',
+    height: '5\' 10"',
+    weight: 190,
+    born: 'January 22, 1996',
+    birthplace: 'Mississauga, ON, CAN'
+  },
+  {
+    name: 'Jordan Kyrou',
+    number: 33,
+    position: 'center',
+    height: '6\' 0"',
+    weight: 175,
+    born: 'May 5, 1998',
+    birthplace: 'Toronto, ON, CAN'
+  },
+  {
+    name: 'Mackenzie MacEachern',
+    number: 62,
+    position: 'left wing',
+    height: '6\' 2"',
+    weight: 190,
+    born: 'March 9, 1994',
+    birthplace: 'Bloomfield, MI, USA'
+  },
+  {
+    name: 'Pat Maroon',
+    number: 7,
+    position: 'left wing',
+    height: '6\' 3"',
+    weight: 225,
+    born: 'April 23, 1988',
+    birthplace: 'St. Louis, MO, USA'
+  },
+  {
+    name: 'Jordan Nolan',
+    number: 71,
+    position: 'center',
+    height: '6\' 3"',
+    weight: 219,
+    born: 'June 23, 1989',
+    birthplace: 'Garden River, First Nations, ON, CAN'
+  },
+  {
+    name: 'Ryan O\'Reilly',
+    number: 90,
+    position: 'center',
+    height: '6\' 1"',
+    weight: 216,
+    born: 'February 7, 1991',
+    birthplace: 'Clinton, ON, CAN'
+  },
+  {
+    name: 'David Perron',
+    number: 57,
+    position: 'left wing',
+    height: '6\' 0"',
+    weight: 200,
+    born: 'May 28, 1988',
+    birthplace: 'Sherbrooke, QC, CAN'
+  },
+  {
+    name: 'Zach Sanford',
+    number: 12,
+    position: 'left wing',
+    height: '6\' 4"',
+    weight: 207,
+    born: 'November 9, 1994',
+    birthplace: 'Salem, MA, USA'
+  },
+  {
+    name: 'Brayden Schenn',
+    number: 10,
+    position: 'center',
+    height: '6\' 1"',
+    weight: 200,
+    born: 'August 22, 1991',
+    birthplace: 'Saskatoon, SK, CAN'
+  },
+  {
+    name: 'Jaden Schwartz',
+    number: 17,
+    position: 'left wing',
+    height: '5\' 10"',
+    weight: 190,
+    born: 'June 25, 1992',
+    birthplace: 'Wilcox, SK, CAN'
+  },
+  {
+    name: 'Alexander Steen (A)',
+    number: 20,
+    position: 'left wing',
+    height: '6\' 0"',
+    weight: 211,
+    born: 'March 1, 1984',
+    birthplace: 'Winnipeg, MB, CAN'
+  },
+  {
+    name: 'Oskar Sundqvist',
+    number: 70,
+    position: 'center',
+    height: '6\' 3"',
+    weight: 209,
+    born: 'March 23, 1994',
+    birthplace: 'Boden, SWE'
+  },
+  {
+    name: 'Vladimir Tarasenko (A)',
+    number: 91,
+    position: 'right wing',
+    height: '6\' 0"',
+    weight: 225,
+    born: 'December 13, 1991',
+    birthplace: 'Yaroslavl, RUS'
+  },
+  {
+    name: 'Robert Thomas',
+    number: 18,
+    position: 'center',
+    height: '6\' 0"',
+    weight: 188,
+    born: 'July 2, 1999',
+    birthplace: 'Aurora, ON, CAN'
+  },
+  {
+    name: 'Chris Thorburn',
+    number: 22,
+    position: 'right wing',
+    height: '6\' 3"',
+    weight: 235,
+    born: 'June 3, 1983',
+    birthplace: 'Sault Ste. Marie, ON, CAN'
+  },
+  {
+    name: 'Robert Bortuzzo',
+    number: 41,
+    position: 'Defense',
+    height: '6\' 4"',
+    weight: 216,
+    born: 'March 18, 1989',
+    birthplace: 'Thunder Bay, ON, CAN'
+  },
+  {
+    name: 'Jay Bouwmeester',
+    number: 19,
+    position: 'Defense',
+    height: '6\' 4"',
+    weight: 206,
+    born: 'September 27, 1983',
+    birthplace: 'Edmonton, AB, CAN'
+  },
+  {
+    name: 'Chris Butler',
+    number: 25,
+    position: 'Defense',
+    height: '6\' 1"',
+    weight: 196,
+    born: 'DOctober 27, 1986',
+    birthplace: 'St. Louis, MO, USA'
+  },
+  {
+    name: 'Michael Del Zotto',
+    number: 42,
+    position: 'Defense',
+    height: '6\' 0"',
+    weight: 201,
+    born: 'June 24, 1990',
+    birthplace: 'Stouffville, ON, CAN'
+  },
+  {
+    name: 'Vince Dunn',
+    number: 29,
+    position: 'Defense',
+    height: '6\' 0"',
+    weight: 203,
+    born: 'October 29, 1996',
+    birthplace: 'Miisissauga, ON, CAN'
+  },
+  {
+    name: 'Joel Edmundson',
+    number: 6,
+    position: 'Defense',
+    height: '6\' 4"',
+    weight: 215,
+    born: 'June 28, 1993',
+    birthplace: 'Brandon, MB, CAN'
+  },
+  {
+    name: 'Carl Gunnarsson',
+    number: 4,
+    position: 'Defense',
+    height: '6\' 2"',
+    weight: 198,
+    born: 'November 9, 1986',
+    birthplace: 'Orebro, SWE'
+  },
+  {
+    name: 'Colton Parayko',
+    number: 55,
+    position: 'Defense',
+    height: '6\' 6"',
+    weight: 230,
+    born: 'May 12, 1993',
+    birthplace: 'St. Albert, AB, CAN'
+  },
+  {
+    name: 'Alex Pietrangelo (C)',
+    number: 27,
+    position: 'Defense',
+    height: '6\' 3"',
+    weight: 210,
+    born: 'January 18, 1990',
+    birthplace: 'King City, ON, CAN'
+  },
+  {
+    name: 'Jake Allen',
+    number: 34,
+    position: 'Goalie',
+    height: '6\' 2"',
+    weight: 203,
+    born: 'August 7, 1990',
+    birthplace: 'Fredericton, NB, CAN'
+  },
+  {
+    name: 'Jordan Binnington',
+    number: 50,
+    position: 'Goalie',
+    height: '6\' 1"',
+    weight: 174,
+    born: 'July 11, 1993',
+    birthplace: 'Richmond Hill, ON, CAN'
+  },
+  {
+    name: 'Ville Husso',
+    number: 35,
+    position: 'Goalie',
+    height: '6\' 3"',
+    weight: 205,
+    born: 'February 6, 1995',
+    birthplace: 'Helsinki, FIN'
+  }
+]
