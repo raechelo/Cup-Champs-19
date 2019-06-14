170929d44075021aa8a5ddb7c4c495c1a033b171
raechel <odom.raechel@gmail.com>
2019-06-14 13:12:02 -0600

    worked with confetti to add to base page, difficult to change size

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import Confetti from 'react-confetti';
 import Cup from '../assets/cup.gif';
-import { useWindowSize } from 'react-use-size';
 
 
 const Home = () => {
-  const { height, width } = useWindowSize()
   return (
     <section className="Home">
-      <Confetti
-        height={height}
-        width={width}
-        className='confetti'
-        />
       <h2>2019 Stanley Cup Champions!</h2>
       <img src={Cup} className="Cup" />
     </section>
